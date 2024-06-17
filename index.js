364823b25731824781ed4a1d805a9eb7e0f3bb6e
Kunark Khewal <kunark.khewal88@gmail.com>
2024-06-17 12:48:34 +0530

    basic agenda setup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const agenda = require("./agendaInit");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
