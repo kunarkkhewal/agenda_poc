f83b0f675abbd88b4ef2d1020e761860a0a8af2a
Kunark Khewal <kunark.khewal88@gmail.com>
2024-06-18 11:25:24 +0530

    added code for scheduled messages and canceling the scheduled jobs

--- workflow.js
+++ workflow.js
@@ -1,20 +1,50 @@
 const { agenda } = require('./agendaInit');
+const amqplib = require('amqplib');
+const moment = require('moment');
+const { ObjectId } = require('mongodb');
 
-
-const printConsole = () => {
-    console.log('Time is : ', new Date());
+const opt = {
+    credentials: amqplib.credentials.plain(
+        process.env.RABBIT_MQ_USERNAME,
+        process.env.RABBIT_MQ_PASSWORD
+    ),
 };
 
-(async function () {
-    try {
+const sendToQueue = async (data) => {
+    const queueName = 'scheduler-queue-kunark';
+    const connection = await amqplib.connect(process.env.RABBIT_MQ_HOST, opt);
+    const channel = await connection.createChannel();
 
-        // Define the job
-        agenda.define('send message', async (job) => {
-            console.log(' ---- job.attrs => ', job.attrs);
-            const { message, recipient } = job.attrs.data;
-            console.log(`Sending message: "${message}" to ${recipient}`);
-            printConsole();
-            // Here you would implement your actual message sending logic, e.g., using an email or SMS service
+    try {
+        // logger.info(`inside sendCheckStatusApptsToQueue`);
+        await channel.assertQueue(queueName, { durable: true });
+        const optional = { persistent: true };
+        await channel.sendToQueue(
+            queueName,
+            Buffer.from(JSON.stringify(data)),
+            optional
+        );
+    } catch (error) {
+        console.log(' ----- Error : ', error);
+        // logger.warning(`error in sendCheckStatusApptsToQueue: ${error}`);
+    } finally {
+        await channel.close();
+        await connection.close();
+    }
+};
+
+// const printConsole = () => {
+//     console.log('Time is : ', new Date());
+// };
+
+const sendThrottleJobToQueue = async (message, time) => {
+    const jobName = 'send scheduled message to queue'
+    try {
+         // Define the job
+        agenda.define(jobName, async (job) => {
+            const message = job.attrs.data;
+            console.log(`Sending message: "${message}"`);
+            await sendToQueue(message);
         });
 
         // Start Agenda
@@ -22,10 +52,11 @@ const printConsole = () => {
         console.log('Agenda started');
 
         // Create and schedule the job
-        const job1 = agenda.create('send message', { message: 'Hello, world!', recipient: 'user@example.com' });
-        await job1.repeatEvery('in 5 seconds');
-        await job1.save();
-
+        const job = agenda.create(jobName, message);
+        await job.schedule(time);
+        await job.save();
+        const jobId = job.attrs._id.toString()
+        console.log('-jobId => ', jobId)
         console.log('Job scheduled');
 
         // Graceful shutdown
@@ -38,8 +69,67 @@ const printConsole = () => {
 
         process.on('SIGTERM', graceful);
         process.on('SIGINT', graceful);
+
+        return jobId;
+    } catch (error) {
+        // console.log('Error in define job')
+        console.error('Error:', error);
+        process.exit(1);
+    }
+}
+
+const deleteJobById = async (jobId) => {
+    try {
+        // Convert jobId to ObjectId
+        const objectId = new ObjectId(jobId);
+        console.log(' --- objectId => ', objectId)
+
+        // Cancel the job
+        const numRemoved = await agenda.cancel({ _id: objectId });
+        console.log(`Number of jobs cancelled: ${numRemoved}`);
+
+        // Graceful shutdown
+        const graceful = () => {
+            console.log(' --- stopping gracefully');
+            agenda.stop(() => {
+                client.close(); // Close MongoDB connection
+                process.exit(0);
+            });
+        };
+
+        process.on('SIGTERM', graceful);
+        process.on('SIGINT', graceful);
     } catch (error) {
         console.error('Error:', error);
         process.exit(1);
     }
-})();
+};
+
+(async function() {
+    const time = moment().add(1, 'minute').toDate();
+    const message = {
+        applicationProperties: {
+            type: 'message type'
+        },
+        body: {
+            practiceId: 1,
+            workflowId: 1,
+            workflowName: 'test reminder',
+            triggerConfigId: 1,
+            jobTriggerTime: time,
+            currentDate: new Date(),
+            timezone: 'utc',
+            fileName: 'utc/timezone',
+            appointmentsCount: 1,
+            fileNameWithArrivalDate: moment().format(),
+            triggeredFrom: 'poc file',
+        },
+    };
+
+    const jobId = await sendThrottleJobToQueue(message, time)
+
+    setTimeout(async () => {
+        await deleteJobById(jobId)
+    }, 10000)
+})()
+
